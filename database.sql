8c2d683c3a0ea08549d61c3a9da7cf901301cc70
FERNANDO <FERNANDO@FERNANDO-PC>
2016-01-23 18:01:12 +0100

    fechas en bd

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 21-01-2016 a las 09:25:03
+-- Tiempo de generación: 23-01-2016 a las 08:59:46
 -- Versión del servidor: 5.6.26
 -- Versión de PHP: 5.5.30
 
@@ -23,64 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `afiliado`
---
-
-CREATE TABLE IF NOT EXISTS `afiliado` (
-  `id` varchar(9) COLLATE utf8_unicode_ci NOT NULL,
-  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `apellido1` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `apellido2` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `fnac` date NOT NULL,
-  `falta` date NOT NULL,
-  `movil` int(9) NOT NULL,
-  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `id_empresa` int(10) NOT NULL,
-  `id_centro` int(10) NOT NULL,
-  `id_cuota` varchar(2) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `centro`
---
-
-CREATE TABLE IF NOT EXISTS `centro` (
-  `id` int(8) NOT NULL,
-  `id_empresa` int(8) NOT NULL,
-  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `direccion` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `cp` int(5) NOT NULL,
-  `id_municipio` int(3) NOT NULL,
-  `id_sector` int(2) NOT NULL,
-  `telefono` int(9) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `centro`
---
-
-INSERT INTO `centro` (`id`, `id_empresa`, `nombre`, `direccion`, `cp`, `id_municipio`, `id_sector`, `telefono`) VALUES
-(1, 1, 'Container', 'Calle Ángel sopeña', 46036, 1, 1, 987544544),
-(2, 1, 'Manises', 'Manises 33', 46111, 2, 1, 777888444),
-(3, 1, 'Quart', 'Quart 33', 46111, 1, 1, 111444888);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cuota`
---
-
-CREATE TABLE IF NOT EXISTS `cuota` (
-  `id` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
-  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `importe` double(2,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `empresa`
 --
 
@@ -88,176 +30,29 @@ CREATE TABLE IF NOT EXISTS `empresa` (
   `id` int(10) NOT NULL,
   `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `cif` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `alta` date DEFAULT NULL,
+  `cambio` date DEFAULT NULL,
   `contacto` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
   `telefono` int(9) NOT NULL,
   `id_usuario` int(2) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `empresa`
 --
 
-INSERT INTO `empresa` (`id`, `nombre`, `cif`, `contacto`, `telefono`, `id_usuario`) VALUES
-(1, 'Mototecnia', '58694673', 'Calle isla cabrera', 684505476, 1),
-(2, 'Colacocaf', '464647777', 'Paquillo', 444888444, 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `liquidacion`
---
-
-CREATE TABLE IF NOT EXISTS `liquidacion` (
-  `id` int(10) NOT NULL,
-  `id_periodo` int(6) NOT NULL,
-  `fliquidacion` date NOT NULL,
-  `importe` double(5,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `municipio`
---
-
-CREATE TABLE IF NOT EXISTS `municipio` (
-  `id` int(3) NOT NULL,
-  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `id_provincia` int(2) NOT NULL,
-  `provincia` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `id_comarca` int(2) NOT NULL,
-  `comarca` varchar(100) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `municipio`
---
-
-INSERT INTO `municipio` (`id`, `nombre`, `id_provincia`, `provincia`, `id_comarca`, `comarca`) VALUES
-(1, 'Valencia', 1, 'Valencia', 1, '1'),
-(2, 'Manises', 1, 'Valencia', 1, 'Valencia');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `pago`
---
-
-CREATE TABLE IF NOT EXISTS `pago` (
-  `id` int(10) NOT NULL,
-  `fecha_pago` date NOT NULL,
-  `id_afiliado` varchar(9) COLLATE utf8_unicode_ci NOT NULL,
-  `id_cuota` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
-  `id_periodo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `periodo`
---
-
-CREATE TABLE IF NOT EXISTS `periodo` (
-  `id` int(6) NOT NULL,
-  `descripcion` varchar(100) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `sector`
---
-
-CREATE TABLE IF NOT EXISTS `sector` (
-  `id` int(2) NOT NULL,
-  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `sector`
---
-
-INSERT INTO `sector` (`id`, `nombre`) VALUES
-(1, 'Motor');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
-
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` int(6) NOT NULL,
-  `login` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `descripcion` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`id`, `login`, `password`, `descripcion`) VALUES
-(1, 'ugt', 'ugt', NULL);
+INSERT INTO `empresa` (`id`, `nombre`, `cif`, `alta`, `cambio`, `contacto`, `telefono`, `id_usuario`) VALUES
+(1, 'Mototecnia', '58694673', '2016-01-12', '2016-01-15', 'Calle isla norrisa', 684505476, 1),
+(2, 'Colacocaf', '464647777', '2016-01-03', '2016-01-14', 'Paquillo', 444888444, 1);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `afiliado`
---
-ALTER TABLE `afiliado`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
-
---
--- Indices de la tabla `centro`
---
-ALTER TABLE `centro`
-  ADD PRIMARY KEY (`id`,`id_empresa`);
-
---
--- Indices de la tabla `cuota`
---
-ALTER TABLE `cuota`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indices de la tabla `empresa`
 --
 ALTER TABLE `empresa`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `municipio`
---
-ALTER TABLE `municipio`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `pago`
---
-ALTER TABLE `pago`
-  ADD PRIMARY KEY (`id_afiliado`,`id_periodo`),
-  ADD KEY `id_pago` (`id`);
-
---
--- Indices de la tabla `periodo`
---
-ALTER TABLE `periodo`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `sector`
---
-ALTER TABLE `sector`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -268,17 +63,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `empresa`
 --
 ALTER TABLE `empresa`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT de la tabla `pago`
---
-ALTER TABLE `pago`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
